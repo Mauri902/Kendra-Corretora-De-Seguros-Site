7898b5f2f51e5a60aef56ff05424d01d669e3b37
Diegoe de Oliveira Galdino <dgaldino2007@gmail.com>
2026-05-08 18:07:45 -0300

    Atualização

--- src/main/webapp/menu.js
+++ src/main/webapp/menu.js
@@ -1,11 +1,20 @@
 $('.seguros').click(function(){
     $('.submenuSeguros').toggleClass('mostra');
+    $('.submenuSobre').removeClass('mostra');
 });
 
 $('.sobre').click(function(){
     $('.submenuSobre').toggleClass('mostra');
+    $('.submenuSeguros').removeClass('mostra');
 });
 
 $('.btnAbre').click(function(){
     $('#menuprincipal').toggleClass('mostra');
 });
+
+$(window).on('scroll', function(){
+    $('#menuprincipal').removeClass('mostra')  
+    $('.submenuSeguros').removeClass('mostra');
+    $('.submenuSobre').removeClass('mostra');
+});
+
